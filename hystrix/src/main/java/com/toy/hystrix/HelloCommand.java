ad0255868a010baad3e794e3868b1405fcd674eb
tany <tanxiangya@xiaomi.com>
2020-04-19 16:30:39 +0800

    增加ignore

--- hystrix/src/main/java/com/toy/hystrix/HelloCommand.java
+++ hystrix/src/main/java/com/toy/hystrix/HelloCommand.java
@@ -28,19 +28,30 @@ public class HelloCommand extends HystrixCommand<String> {
     }
 
     public static void main(String[] args) {
-        HelloCommand command = new HelloCommand("hystrix-group");
-        String result = command.execute();
+//        HelloCommand command = new HelloCommand("hystrix-group");
+//        String result = command.execute();
+//
+//        System.out.println(" halo " + result);
+//
+//        HelloCommand asyCommand = new HelloCommand("hystrix-gg");
+//        Future<String> future = asyCommand.queue();
+//        try {
+//            String rr = future.get();
+//            System.out.println(" halo, halo " + rr);
+//        } catch (Exception e) {
+//            System.out.println(" catch error ");
+//        }
+        String i = "i";
+        String i1 = new String("i");
+        StringBuilder sb = new StringBuilder(i1);
+        sb.reverse();
 
-        System.out.println(" halo " + result);
+        StringBuffer buffer = new StringBuffer(i);
+        buffer.reverse();
 
-        HelloCommand asyCommand = new HelloCommand("hystrix-gg");
-        Future<String> future = asyCommand.queue();
-        try {
-            String rr = future.get();
-            System.out.println(" halo, halo " + rr);
-        } catch (Exception e) {
-            System.out.println(" catch error ");
-        }
+        StringUtils.reverse(i);
+        i.intern();
+        System.out.println(i.hashCode() + ":" + i1.hashCode());
 
     }
 }
